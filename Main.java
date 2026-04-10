31870e395d08dd3e45171cfa22f121edfbc7e65b
dinislamalipkachev <dalipkachev@inbox.ru>
2026-04-10 11:28:54 +0300

    SELECT name FROM CLIENTS REG_DATE BETWEEN

--- Main.java
+++ Main.java
@@ -8,16 +8,20 @@ class Main {
 
     public static void main(String[] args) {
         String sql = """
-                SELECT * FROM repairs
+                SELECT * FROM clients WHERE registration_date BETWEEN ? AND ?
                 """;
         try (var connection = DriverManager.getConnection(URL,
                 USERNAME, PASSWORD);
              var prepareStatement = connection.prepareStatement(sql)) {
 
+            prepareStatement.setDate(1, Date.valueOf("2010-12-12"));
+            prepareStatement.setDate(2, Date.valueOf("2020-12-12"));
+
+
             var resultSet = prepareStatement.executeQuery();
 
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("vehicle_id"));
+                System.out.println(resultSet.getString("name"));
             }
         } catch (SQLException e) {
             System.out.println("Ex: " + e.getMessage());
