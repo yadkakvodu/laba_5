dfb29ccba6c2b14395edc89ec886cad13eba939a
dinislamalipkachev <dalipkachev@inbox.ru>
2026-04-10 11:09:55 +0300

    SELECT * FROM clients

--- Main.java
+++ Main.java
@@ -1,19 +1,15 @@
 
 import java.sql.*;
-
 class Main {
 
     private static final String PASSWORD = "Osman200202";
     private static final String USERNAME = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/rent_of_transport_equipment";
 
-
     public static void main(String[] args) throws SQLException {
-
         String sql = """
                 SELECT * FROM clients
                 """;
-
         try (var connection = DriverManager.getConnection(URL,
                 USERNAME, PASSWORD);
              var prepareStatement = connection.prepareStatement(sql)) {
@@ -23,10 +19,8 @@ class Main {
             while (resultSet.next()) {
                 System.out.println(resultSet.getString("payment_status"));
             }
-
         } catch (SQLException e) {
             System.out.println("Ex: " + e.getMessage());
         }
-
     }
 }
